a0310418a600a329ccd00fb211c13cca70927e49
DanielAndradeViana <vianad07@gmail.com>
2022-01-24 20:59:44 -0300

    first commit

---
+++ dio/calculadora.java
@@ -0,0 +1,14 @@
+package dio;
+
+public class calculadora {
+    public static void soma(double numero1, double numero2){
+
+        double resultado = numero1 + numero2;
+
+        System.out.println("a soma de" + numero1 + "mais" + numero2 +"é" + resultado);
+
+    
+}
+
+    
+}
---
+++ dio/main.java
@@ -0,0 +1,13 @@
+package dio;
+
+public class main {
+    public static void main(String[] args) {
+
+        // Calculadora
+        System.out.println("exercicio Calculadora");
+        calculadora.soma(3, 6);
+        
+    }
+    
+    
+}
